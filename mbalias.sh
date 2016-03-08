02454760aac3d78702ff2926af98af0d03e19a40
Michael Beasley <youvegotmoxie@gmail.com>
2016-03-08 17:57:57 -0500

    modified:   mbalias.sh - zzmysqltuneup fixes

--- mbalias.sh
+++ mbalias.sh
@@ -157,11 +157,13 @@ zzmysqltune() {
 }
 
 zzmysqltuneup() {
-    screen -S ticket_$1
     mkdir -p /home/.hd/ticket/$1/logs
-    for db in $(find /var/lib/mysql -type f -name '*.MYI');do myisamchk -r $db;done | tee -a /home/.hd/ticket/$1/logs/myisamchk-$(date +%s).log
-    mysqlcheck -rA | tee -a /home/.hd/ticket/$1/logs/mysqlcheck-$(date +%s).log
-    mysqlcheck -oA | tee -a /home/.hd/ticket/$1/logs/mysqlcheck-optimize-$(date +%s).log
+    screen -S ticket_$1 -d -m /bin/bash -c 'echo -e "\nmyisamchk\n"'
+      for db in $(find /var/lib/mysql -type f -name '*.MYI'); do myisamchk -r $db; done | tee -a /home/.hd/ticket/$1/logs/mysqlcheck-$(date +%s).log
+      echo -e "\nmysqlcheck repair\n"
+      mysqlcheck -rA | tee -a /home/.hd/ticket/$1/logs/mysqlcheck-$(date +%s).log
+      echo -e "\nmysqlcheck optimize\n"
+      mysqlcheck -oA | tee -a /home/.hd/ticket/$1/logs/mysqlcheck-optimize-$(date +%s).log
 }
 
 zzapachetune() {
