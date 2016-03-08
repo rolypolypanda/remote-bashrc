5d82f758a92fb807933566bd0a542ba0144934eb
Michael Beasley <youvegotmoxie@gmail.com>
2016-03-07 19:50:30 -0500

    modified: mbalias.sh - fixed syntax error with tr command

--- mbalias.sh
+++ mbalias.sh
@@ -203,10 +203,10 @@ zzcmsdbinfo() {
  CMS="$1"
  case $CMS in
    --wordpress)
-      DB_NAME="$(grep DB_NAME wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d')' | tr -d ';')"
-      DB_USER="$(grep DB_USER wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d')' | tr -d ';')"
-      DB_PASS="$(grep DB_PASSWORD wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d')' | tr -d ';')"
-      TBL_PREFIX="$(grep table_prefix wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d')' | tr -d ';')"
+      DB_NAME="$(grep DB_NAME wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d ')' | tr -d ';')"
+      DB_USER="$(grep DB_USER wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d ')' | tr -d ';')"
+      DB_PASS="$(grep DB_PASSWORD wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d ')' | tr -d ';')"
+      TBL_PREFIX="$(grep table_prefix wp-config.php | awk '{ print $2}' | tr -d "'" | tr -d ')' | tr -d ';')"
       echo "Database Name: ${DB_NAME}"
       echo "Database User: ${DB_USER}"
       echo "Database Password: ${DB_PASS}"
