d2526accbd51290a9df1d4f2bf59d179b6abe46b
Michael Beasley <youvegotmoxie@gmail.com>
2016-03-08 20:48:55 -0500

    modified:   mbalias.sh - fixed logging for zzmysqltuneup

--- mbalias.sh
+++ mbalias.sh
@@ -161,9 +161,9 @@ zzmysqltuneup() {
     sleep 5 ;
     read -p "Enter ticket ID number: " TID
     mkdir -p /home/.hd/ticket/$TID/logs ;
-    for db in $(find /var/lib/mysql -type f -name "*.MYI"); do myisamchk -r $db; done | tee /home/.hd/ticket/$TID/logs/myisamchk-repair-$(date +%s).log ;
-    mysqlcheck -rA | tee /home/.hd/ticket/$TID/logs/mysqlcheck-repair-$(date +%s).log ;
-    mysqlcheck -oA | tee /home/.hd/ticket/$TID/logs/mysqlcheck-optimize-$(date +%s).log ;
+    for db in $(find /var/lib/mysql -type f -name "*.MYI"); do myisamchk -r $db; done | tee -a /home/.hd/ticket/$TID/logs/myisamchk-repair-$(date +%s).log ;
+    mysqlcheck -rA | tee -a /home/.hd/ticket/$TID/logs/mysqlcheck-repair-$(date +%s).log ;
+    mysqlcheck -oA | tee -a /home/.hd/ticket/$TID/logs/mysqlcheck-optimize-$(date +%s).log ;
     wall -n "MySQL table repair and optimize complete." ;
     wall -n "Log located in \`/home/.hd/ticket/$TID/logs/mysqlcheck-repair-$(date +%s).log\`" ;
     wall -n "Log located in \`/home/.hd/ticket/$TID/logs/myisamchk-repair-$(date +%s).log\`" ;
