9580be4a00c44b175b72a4af18946b75ca6686e3
Mike B <michael.b@hostdime.com>
2016-03-24 08:53:48 -0400

    modified: removed colors, added more notes to zzbackuprest

--- mbalias.sh
+++ mbalias.sh
@@ -164,14 +164,14 @@ zzfixtmp() {
 
 zzacctdom() {
     if [[ $(egrep -w $1  /etc/trueuserdomains | cut -d':' -f2 | sed -e 's/^[ \t]*//' | while read list;do grep -cw $list /var/cpanel/resellers;done | cut -d':' -f1) == 1 ]];then
-        echo -e "Reseller:$G1 Yes $RS" ;
+        echo -e "Reseller: Yes" ;
         echo -e "Resold Accounts: $(for i in $(grep $1 /etc/trueuserdomains | cut -d':' -f2 | sed -e 's/^[ \t]*//');do grep $i /etc/trueuserowners | cut -d':' -f1 | grep -v $i | wc -l;done)" ;
         echo -e "Account Owner: $(for i in $(egrep -w $1 /etc/trueuserdomains | cut -d':' -f1);do egrep -w ^$i /etc/userdatadomains | grep main | cut -d'=' -f3;done)" ;
         echo -e "Account Name: $(grep $1 /etc/trueuserdomains | cut -d':' -f2 | sed -e 's/^[ \t]*//')" ;
         echo -e "Document Root: $(egrep -w ^$(egrep -w $1 /etc/trueuserdomains | cut -d':' -f1) /etc/userdatadomains | grep main | cut -d'=' -f9)" ;
         echo -e "IP Address: $(egrep -w ^$(egrep -w $1 /etc/trueuserdomains | cut -d':' -f1) /etc/userdatadomains | grep main | cut -d'=' -f11)" ;
     else
-        echo -e "Reseller:$R1 No $RS" ;
+        echo -e "Reseller: No" ;
         echo -e "Account Owner: $(for i in $(grep $1 /etc/trueuserdomains | cut -d':' -f2);do grep $i /etc/trueuserowners | cut -d':' -f2 | sed -e 's/^[ \t]*//';done)" ;
         echo -e "Account Name: $(grep $1 /etc/trueuserdomains | cut -d':' -f2 | sed -e 's/^[ \t]*//')" ;
         echo -e "Document Root: $(grep -w ^$1 /etc/userdatadomains | cut -d'=' -f9)" ;
@@ -192,12 +192,16 @@ zzbackuprest() {
     mkdir -p /home/.hd/logs/$TID/$ACT ;
     echo -e "Copying ${BKP} to /home"
     CPMOVE="$(ls -lah $BKP | rev | cut -d'/' -f1 | rev)"
-    cp -vP $BKP /home/$CPMOVE ;
+    \cp -vP $BKP /home/$CPMOVE ;
     cd /home ;
     /usr/local/cpanel/bin/cpuwatch $(grep -c proc /proc/cpuinfo) /scripts/restorepkg $CPMOVE | tee -a /home/.hd/logs/$TID/$ACT/restorepkg-$(date +%s).log ;
-    rm -f /home/$CPMOVE ;
+    \rm -f /home/$CPMOVE ;
+    echo -e "\n- Copied backup from ${BKP} to \`/home:\`"
+    echo -e "\`[root@$(hostname):$(pwd) #] cp -vP ${BKP} /home/${CPMOVE}\`"
     echo -e "\n- Restored account \`${ACT}:\`"
     echo -e "\`[root@$(hostname):$(pwd) #] /usr/local/cpanel/bin/cpuwatch $(grep -c proc /proc/cpuinfo) /scripts/restorepkg ${CPMOVE}\`"
+    echo -e "\n- Removed backup from \`/home:\`"
+    echo -e "\`[root@$(hostname):$(pwd) #] rm -vf /home/${CPMOVE}\`"
     echo -e "\n- Log located in: \`/home/.hd/logs/$TID/$ACT/restorepkg-$(date +%s).log\`"
 }
 
